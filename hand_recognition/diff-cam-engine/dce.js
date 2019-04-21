d1a6f37c952f03b87961c0aea7bc4ab23a304960
Michal Rychetník <rychet.michal@gmail.com>
2019-04-21 15:13:29 +0200

    diff-cam-eng fixed style and reading

--- hand_recognition/diff-cam-engine/dce.js
+++ hand_recognition/diff-cam-engine/dce.js
@@ -3,8 +3,6 @@ var $motionBox = $('.motion-box');
 
 var scale = 10;	// capture resolution over motion resolution
 var isActivated = false;
-var isTargetInSight = false;
-var isKnockedOver = false;
 var lostTimeout;
 
 function initSuccess() {
@@ -25,7 +23,7 @@ function activate() {
 }
 
 function capture(payload) {
-	if (!isActivated || isKnockedOver) {
+	if (!isActivated) {
 		return;
 	}
 
@@ -45,14 +43,9 @@ function capture(payload) {
 			height: height
 		});
 
-		if (!isTargetInSight) {
-			isTargetInSight = true;
-			//play('i-see-you');
-		}
-
 		var middleX = (box.x.max + box.x.min) / 2;
 		var middleY = (box.y.max + box.y.min) / 2;
-		//document.getElementById("log").innerHTML += "Y=" + middleY + "; ";
+
 		if (middleX > 50) {
 			document.getElementById("log").innerHTML += "LEFT ";
 		}
@@ -68,34 +61,15 @@ function capture(payload) {
 		}
 
 		clearTimeout(lostTimeout);
-		lostTimeout = setTimeout(declareLost, 2000);
+		lostTimeout = setTimeout(2000);
 	}
-
-	// video is flipped, so (0, 0) is at top right
-	if (payload.checkMotionPixel(0, 0)) {
-
-	}
-
 	
-}
-
-function declareLost() {
-	isTargetInSight = false;
-}
-
-function knockOver() {
-	isKnockedOver = true;
-	clearTimeout(lostTimeout);
-
-	
-	$motionBox.hide();
-
 }
 
 
 DiffCamEngine.init({
 	video: document.getElementById('video'),
-	captureIntervalTime: 50,
+	captureIntervalTime: 75,
 	includeMotionBox: true,
 	includeMotionPixels: true,
 	initSuccessCallback: initSuccess,
