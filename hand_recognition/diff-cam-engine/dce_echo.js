0955a610adf16eefafe029770ae131cc50581a0e
Michal Rychetník <rychet.michal@gmail.com>
2019-04-23 11:23:17 +0200

    added dce echo

---
+++ hand_recognition/diff-cam-engine/dce_echo.js
@@ -0,0 +1,127 @@
+var $motionBox = $('.motion-box');
+
+var scale = 10;	// capture resolution over motion resolution
+var isActivated = false;
+var lostTimeout;
+
+var directionTimer = false;
+
+//up = 38, down = 40, left = 37, right = 39
+//up, up, down, down, left, right, left, right, up, down, up, down, left, left, right, right
+var directionScript = [38, 38, 40, 40, 37, 39, 37, 39, 38, 40, 38, 40, 37, 37, 39, 39];
+var currentDir = 0;
+var score = 0;
+//var timer = 0;
+
+function showNext() {
+	document.getElementById("score").innerHTML = score;
+	var dir = "";
+	if (currentDir >= directionScript.length) {
+		document.getElementById("instruction").innerHTML = "FINISHED";
+		stopStopwatch();
+		return;
+	}
+
+	switch(directionScript[currentDir]) {
+		case 37:
+			dir = "LEFT";
+			break;
+		case 39:
+			dir = "RIGHT"
+			break;
+		case 38:
+			dir = "UP";
+			break;
+		case 40:
+			dir = "DOWN"
+			break;
+		default:
+			break;
+	}
+
+	document.getElementById("instruction").innerHTML = dir;
+	
+}
+
+function initSuccess() {
+	DiffCamEngine.start();
+}
+
+function initError() {
+	alert('Something went wrong.');
+}
+
+function startComplete() {
+	setTimeout(activate, 500);
+	showNext();
+	startStopwatch();
+}
+
+function activate() {
+	isActivated = true;
+}
+
+function capture(payload) {
+	if (!isActivated) {
+		return;
+	}
+
+	var box = payload.motionBox;
+	if (box) {
+		// video is flipped, so we're positioning from right instead of left
+		var right = box.x.min * scale + 1;
+		var top = box.y.min * scale + 1;
+		var width = (box.x.max - box.x.min) * scale;
+		var height = (box.y.max - box.y.min) * scale;
+
+		$motionBox.css({
+			display: 'block',
+			right: right,
+			top: top,
+			width: width,
+			height: height
+		});
+
+		if (!directionTimer) {
+			var middleX = (box.x.max + box.x.min) / 2;
+			var middleY = (box.y.max + box.y.min) / 2;
+
+			if (middleX > 50 || middleX < 10 || middleY < 15 || middleY > 40) {
+				directionTimer = true;
+				setTimeout(function() { directionTimer = false; }, 1000);
+				if (middleX > 48 && directionScript[currentDir] == 37) { //left
+					score++;
+				}
+				if (middleX < 12 && directionScript[currentDir] == 39) {	//right
+					score++;
+				}
+				if (middleY < 17 && directionScript[currentDir] == 38) {	//up
+					score++;
+				}
+				if (middleY > 38 && directionScript[currentDir] == 40) {	//down
+					score++;
+				}
+
+				stopwatchLap();
+				currentDir++;
+				showNext();
+			}
+		}
+		
+		clearTimeout(lostTimeout);
+		lostTimeout = setTimeout(2000);
+	}
+	
+}
+
+
+DiffCamEngine.init({
+	video: document.getElementById('video'),
+	captureIntervalTime: 75,
+	includeMotionBox: true,
+	includeMotionPixels: true,
+	initSuccessCallback: initSuccess,
+	initErrorCallback: initError,
+	startCompleteCallback: startComplete,
+	captureCallback: capture
+});
